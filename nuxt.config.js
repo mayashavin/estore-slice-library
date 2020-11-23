73413737fc1432a6b3ffacf60ea5c3d473a48ec7
Maya Shavin <maya@cloudinary.com>
2020-11-23 21:28:13 +0200

    fix: configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 import smConfig from "./sm.json";
 
 if (!smConfig.apiEndpoint) {
-  console.warn("Looks like Slice Machine hasn't been bootstraped already.\nCheck the `Getting Started` section of the README file :)");
+  console.warn(
+    "Looks like Slice Machine hasn't been bootstraped already.\nCheck the `Getting Started` section of the README file :)"
+  );
 }
 
 export default {
@@ -10,59 +12,68 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: "Prismstore Components",
-    meta: [{
-      charset: "utf-8"
-    }, {
-      name: "viewport",
-      content: "width=device-width, initial-scale=1"
-    }, {
-      hid: "description",
-      name: "description",
-      content: "Prismstore slice library"
-    }],
-    link: [{
-      rel: "icon",
-      type: "image/png",
-      href: "/logo.png"
-    }, {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap"
-    }]
+    meta: [
+      {
+        charset: "utf-8"
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      },
+      {
+        hid: "description",
+        name: "description",
+        content: "Prismstore slice library"
+      }
+    ],
+    link: [
+      {
+        rel: "icon",
+        type: "image/png",
+        href:
+          "https://res.cloudinary.com/mayashavin/image/upload/f_auto,q_auto,w_16,c_fit/v1606158086/prismUi/white_logo_color_background"
+      },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap"
+      }
+    ]
   },
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    "plugins/cloudinary"
-  ],
+  plugins: ["plugins/cloudinary"],
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    "@nuxtjs/sitemap",    
-    '@nuxtjs/tailwindcss'
-  ],
+  buildModules: ["@nuxtjs/sitemap", "@nuxtjs/tailwindcss"],
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [["@nuxtjs/prismic", {
-    endpoint: smConfig.apiEndpoint || "",
-    apiOptions: {
-      routes: [{
-        type: "page",
-        path: "/:uid"
-      }]
-    }
-  }], ["nuxt-sm"], ['@nuxtjs/svg-sprite']],
+  modules: [
+    [
+      "@nuxtjs/prismic",
+      {
+        endpoint: smConfig.apiEndpoint || "",
+        apiOptions: {
+          routes: [
+            {
+              type: "page",
+              path: "/:uid"
+            }
+          ]
+        }
+      }
+    ],
+    ["nuxt-sm"],
+    ["@nuxtjs/svg-sprite"]
+  ],
 
   storybook: {
     modules: {
-      exclude: [
-        '@nuxtjs/svg-sprite'
-      ]
+      exclude: ["@nuxtjs/svg-sprite"]
     },
-    stories: [
-      "~/components/**/*.stories.js",
-      "~/slices/**/*.stories.js"
-    ]
+    stories: ["~/components/**/*.stories.js", "~/slices/**/*.stories.js"],
+    addons: ["@storybook/addon-knobs"]
   },
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
@@ -77,9 +88,7 @@ export default {
   tailwindcss: {
     config: {
       purge: {
-        content: [
-          "slices/**/**.vue"
-        ]
+        content: ["slices/**/**.vue"]
       }
     }
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,27 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        rail: ['Raleway', 'sans-serif']
+        rail: ["Raleway", "sans-serif"]
       },
       height: {
-        banner: '25rem'
+        banner: "25rem"
+      },
+      colors: {
+        prismui: {
+          50: "#FBF6FB",
+          100: "#F8ECF8",
+          200: "#ECD0EC",
+          300: "#E1B4E1",
+          400: "#CB7CCB",
+          500: "#B444B4",
+          600: "#A23DA2",
+          700: "#6C296C",
+          800: "#511F51",
+          900: "#361436"
+        }
       }
-    },
+    }
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
